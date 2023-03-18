16048603dc6171b09c5c066568d9a79f2f2ee70a
Marat Azoyan <marat.azoyan1@gmail.com>
2023-03-18 17:26:22 +0400

    first

--- src/components/Controller.jsx
+++ src/components/Controller.jsx
@@ -1,8 +1,8 @@
 const Controller=(props)=>{return(
 
  <div className='controller'>
-  <img  onClick={props.pageUp} className='arrow' src="./images/icon-prev.svg"/>
-  <img  onClick={props.pageDown} className='arrow' src="./images/icon-next.svg"/>
+  <img  onClick={props.pageUp} className='arrow' src="./images/icon-prev.svg" alt="arrow"/>
+  <img  onClick={props.pageDown} className='arrow' src="./images/icon-next.svg" alt="arrow"/>
   </div>
 
 
